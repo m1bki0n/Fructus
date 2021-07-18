06daa24b6d3e5eba8b9b5819014611c7fb2ce32c
Makwan BK <contact@makwanbk.com>
2021-07-18 18:26:00 +0300

    Initial Commit

---
+++ Shared/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Shared
+//
+//  Created by Makwan BK on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
